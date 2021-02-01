52fbe1bdbebb1e7b1e0d71e6ff373419d1f2ab15
Hue Pham <huepham113@gmail.com>
2021-01-31 23:50:23 -0500

    Added protect() in authController; Added method to handle JWT error/expire in errorController; Added userSchema.methods to compare JWTTimesStamp vs passwordChangedTimeStamp

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log('req.headers', req.headers);
   next();
 });
 
--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: Date,
 });
 
 // encrypt password before saving to database
@@ -49,6 +50,17 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // False means NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema); // name of model, schema
