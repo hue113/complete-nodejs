41a07fc2239cb8dde8e986ac7e65a4be810c4ac4
Hue Pham <huepham113@gmail.com>
2021-02-06 02:27:03 -0500

    Built LogIn Page & Connected to API; Used parcel to build bundle

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const viewRouter = express.Router();
 
+viewRouter.use(authController.isLoggedIn);
+
 viewRouter.get('/', viewsController.getOverview);
-viewRouter.get('/tour/:slug', viewsController.getTour);
+viewRouter.get('/tour/:slug', authController.protect, viewsController.getTour);
 viewRouter.get('/login', viewsController.getLoginForm);
 
 module.exports = viewRouter;
