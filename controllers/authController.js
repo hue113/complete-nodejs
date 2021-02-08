811b01d91d1071c111304567b3e32f2b71cfe395
Hue Pham <huepham113@gmail.com>
2021-02-08 11:54:32 -0500

    Added email handlers in utils; Implemented pug/email templates to send welcome emails and password reset email

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const { promisify } = require('util');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 // eslint-disable-next-line arrow-body-style
 const signToken = (id) => {
@@ -40,12 +40,18 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-  // name: req.body.name,
-  // email: req.body.email,
-  // password: req.body.password,
-  // passwordConfirm: req.body.passwordConfirm,
-  // passwordChangedAt: req.body.passwordChangedAt,
+  // const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  // const url = `${req.protocol}://localhost:3000/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -172,13 +178,16 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   // 3) Send token back to user's email
   const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+  // eslint-disable-next-line max-len
+  // const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message,
+    // });
+    await new Email(user, resetURL).sendPasswordReset();
+
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!',
