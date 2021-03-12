af1f0c55b607b4081ea75ac59bdec436a6ff6a7f
Hue Pham <huepham113@gmail.com>
2021-03-12 13:57:25 -0500

    fixed part29 with bundle

---
+++ public/js/update.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+const { showAlert } = require('./alerts');
+
+module.exports.updateSettings = async (data, type) => {
+  try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/updateMyPassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
+    const res = await axios({
+      method: 'PATCH',
+      url,
+      data,
+    });
+    console.log('res', res);
+    if (res.data.status === 'success') {
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
+    }
+  } catch (err) {
+    // console.log(err.response);
+    showAlert('error', err.response.data.message);
+  }
+};
